e8ff76d617ab5dd776168a7bd092357034aefeee
David <kadowki@gmail.com>
2014-07-11 10:35:22 -0500

    Added area, volume functions

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 var prompt = require('sync-prompt').prompt;
 
 
-function increment(x){
-	return x += 1;
-}
-function square(x){
-	return x * x;
-}
+function increment(x){ return x += 1; }
+function square(x){ return x * x;}
+function area(length, width){ return length * width; }
+
+function volume(l, w, h){ return area(l, w) * h; }
+//Incremenet function
 var b = increment(5);
-console.log(b);
+console.log("Increment of 5: " + b);
+//Square function
 b = square(5);
-console.log(b);
+console.log("Square of 5: " + b);
 
+//Area function
+console.log(area(50, 50));
 
+//Volum function
+console.log(volume(50, 50, 10));
